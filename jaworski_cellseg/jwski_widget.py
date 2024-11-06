948ed7107663259d91ad64616301690253d267b6
Camilo Diaz <k.diaz99@gmail.com>
2024-11-06 13:10:22 -0500

    STY: format python files

--- jaworski_cellseg/jwski_widget.py
+++ jaworski_cellseg/jwski_widget.py
@@ -8,13 +8,13 @@ from .pre_process_data_widget import create_pre_process_data_widget
 from .label_counting_widget import create_label_counting_widget
 import torch
 
+
 class JaworskiWidget(QWidget):
-    def __init__(self, napari_viewer: 'napari.viewer.Viewer', parent=None):
+    def __init__(self, napari_viewer: "napari.viewer.Viewer", parent=None):
         super().__init__(parent)
         self.viewer = napari_viewer
-        self.physical_sizes = {"x": 0.0, "y": 0.0, "z":0.0}
+        self.physical_sizes = {"x": 0.0, "y": 0.0, "z": 0.0}
         self.init_ui()
-        
 
     def init_ui(self):
         # Create the main widget to hold both widgets
@@ -22,11 +22,13 @@ class JaworskiWidget(QWidget):
         inferer_widget = Inferer(self.viewer)
         # Create each widget, passing the viewer to each
         data_widget = create_load_data_widget(self.viewer, self.physical_sizes)
-        pre_process_data_widget = create_pre_process_data_widget(self.viewer, inferer_widget)
-        count_widget = create_label_counting_widget(self.viewer,self.physical_sizes)
+        pre_process_data_widget = create_pre_process_data_widget(
+            self.viewer, inferer_widget
+        )
+        count_widget = create_label_counting_widget(self.viewer, self.physical_sizes)
 
         # # Add both widgets to the main layout
-        layout.addWidget(data_widget.native) 
+        layout.addWidget(data_widget.native)
         layout.addWidget(pre_process_data_widget.native)
         layout.addWidget(count_widget.native)
 
@@ -41,11 +43,11 @@ class JaworskiWidget(QWidget):
         inferer_widget.thresholding_slider.setValue(60)
 
         inferer_widget.use_instance_choice.setChecked(True)
-        inferer_widget.instance_widgets.methods["Voronoi-Otsu"].counters[0].setValue(2.5)
-        inferer_widget.instance_widgets.methods["Voronoi-Otsu"].counters[2].setValue(25.00)
+        inferer_widget.instance_widgets.methods["Voronoi-Otsu"].counters[0].setValue(
+            2.5
+        )
+        inferer_widget.instance_widgets.methods["Voronoi-Otsu"].counters[2].setValue(
+            25.00
+        )
 
         layout.addWidget(inferer_widget)
-        
-
-
-    
--- jaworski_cellseg/label_counting_widget.py
+++ jaworski_cellseg/label_counting_widget.py
@@ -4,47 +4,48 @@ from skimage.measure import regionprops
 import pandas as pd
 
 
-def generate_statistics(regions , physical_sizes, save_file_path):
+def generate_statistics(regions, physical_sizes, save_file_path):
     # Initialize a list to hold the properties data
     data = []
-  
+
     # Loop through each region and filter for the labels of interest
     for region in regions:
-            # Extract properties and add them to the data list as a dictionary
-            region_data = {
-                'label': region.label,
-                'area': region.area,
-                'centroid_row': region.centroid[2],
-                'centroid_col': region.centroid[1],
-                'centroid_depth': region.centroid[0],
-                'bbox': region.bbox  # Add more properties as needed
-            }
+        # Extract properties and add them to the data list as a dictionary
+        region_data = {
+            "label": region.label,
+            "area": region.area,
+            "centroid_row": region.centroid[2],
+            "centroid_col": region.centroid[1],
+            "centroid_depth": region.centroid[0],
+            "bbox": region.bbox,  # Add more properties as needed
+        }
 
-            
-            min_slice, min_row, min_col, max_slice, max_row, max_col  = region.bbox
-            
-            # Calculate the center pixel of the bounding box
-            center_pixel = (
-                (min_col + max_col) // 2,
-                (min_row + max_row) // 2,
-                (min_slice + max_slice) // 2
-            )
-            region_data["center_x"]= center_pixel[0]
-            region_data["center_y"]= center_pixel[1]
-            region_data["center_z"]= center_pixel[2]
+        min_slice, min_row, min_col, max_slice, max_row, max_col = region.bbox
 
+        # Calculate the center pixel of the bounding box
+        center_pixel = (
+            (min_col + max_col) // 2,
+            (min_row + max_row) // 2,
+            (min_slice + max_slice) // 2,
+        )
+        region_data["center_x"] = center_pixel[0]
+        region_data["center_y"] = center_pixel[1]
+        region_data["center_z"] = center_pixel[2]
 
-            voxel_count = region.area  # This is the number of voxels in the region
-            voxel_volume = physical_sizes["x"] * physical_sizes["y"] * physical_sizes["z"]  # Volume of a single voxel in µm³
-            total_volume_um3 = voxel_count * voxel_volume  # Total volume in µm³
+        voxel_count = region.area  # This is the number of voxels in the region
+        voxel_volume = (
+            physical_sizes["x"] * physical_sizes["y"] * physical_sizes["z"]
+        )  # Volume of a single voxel in µm³
+        total_volume_um3 = voxel_count * voxel_volume  # Total volume in µm³
 
-            region_data['volume'] = total_volume_um3
+        region_data["volume"] = total_volume_um3
 
-            data.append(region_data)
+        data.append(region_data)
     # Convert the data list to a DataFrame
     df = pd.DataFrame(data)
     # Save the DataFrame to a CSV file
     df.to_csv(save_file_path, index=False)
+
 
 def create_label_counting_widget(viewer: napari.Viewer, physical_sizes: dict):
     @magicgui(
@@ -70,27 +71,25 @@ def create_label_counting_widget(viewer: napari.Viewer, physical_sizes: dict):
         label_layer_name: str = None,
         binary_layer_name: str = None,
         total_count: str = "",
-        save_results: str = None
+        save_results: str = None,
     ):
         labeled_layer = viewer.layers[label_layer_name]
         binary_mask = viewer.layers[binary_layer_name]
-        
+
         ## filter lables outside the binary mask
         result_image = labeled_layer.data * binary_mask.data
         filtered_labels_regions = regionprops(result_image)
         count = len(filtered_labels_regions)
         cell_counting_widget.total_count.value = str(count)
-        
+
         viewer.add_labels(result_image, name=f"{count} Filtered Labled layer")
 
         if cell_counting_widget.save_results.value:
             file_path = cell_counting_widget.save_results.value
-            with open(file_path, 'w') as f:
+            with open(file_path, "w") as f:
                 generate_statistics(filtered_labels_regions, physical_sizes, file_path)
-                
-            viewer.status = f"Results saved to {file_path}"
 
-        
+            viewer.status = f"Results saved to {file_path}"
 
     def update_layer_dropdown(event=None):
         # Get names of all label layers in the viewer
