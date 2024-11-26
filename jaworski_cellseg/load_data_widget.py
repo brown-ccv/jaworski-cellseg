maint: fixed problem loading physical units in tif files

jaworski_cellseg/load_data_widget.py
@@ -5,45 +5,57 @@ from bioio import BioImage
 import tifffile
 import numpy as np
 
-def jwslab_load_bio_data(napari_viewer: napari.Viewer, file_path: Path,
-                         physical_sizes: dict):
+
+def jwslab_load_bio_data(
+    napari_viewer: napari.Viewer, file_path: Path, physical_sizes: dict
+):
     try:
         file_extension = file_path.suffix.lower()
         if file_extension == ".tiff" or file_extension == ".tif":
             # Load TIFF file using tifffile
             bio_data = tifffile.imread(file_path)
             transformed_data = np.moveaxis(bio_data, 1, 0)
-            napari_viewer.add_image(transformed_data,channel_axis=0,
-                name=[f"Channel {i}" for i in range(bio_data.shape[0])])
+            napari_viewer.add_image(
+                transformed_data,
+                channel_axis=0,
+                name=[f"Channel {i}" for i in range(bio_data.shape[0])],
+            )
             ## get physical size of the data
             with tifffile.TiffFile(file_path) as tiff:
                 page = tiff.pages[0]
-                 # Extract resolution-related tags
+                # Extract resolution-related tags
                 x_resolution = page.tags.get("XResolution", None)
                 y_resolution = page.tags.get("YResolution", None)
-                
+
                 unit_name = None
-                z_physical_size  = 1.0
+                z_physical_size = 1.0
                 if tiff.imagej_metadata:
-                    unit_name = tiff.imagej_metadata.unit
+                    unit_name = tiff.imagej_metadata["unit"]
                     if "spacing" in tiff.imagej_metadata:
                         z_physical_size = tiff.imagej_metadata["spacing"]
 
+                print(x_resolution)
+                print(y_resolution)
                 # Decode values
                 if x_resolution and y_resolution:
-                    x_res = x_resolution.value 
+                    x_res = x_resolution.value
                     y_res = y_resolution.value
-                     
+
                     # physical size (numerator, denominator)
-                    x_res_value = x_res[0] / x_res[1] if isinstance(x_res, tuple) else x_res
-                    y_res_value = y_res[0] / y_res[1] if isinstance(y_res, tuple) else y_res
+                    x_res_value = (
+                        x_res[0] / x_res[1] if isinstance(x_res, tuple) else x_res
+                    )
+                    y_res_value = (
+                        y_res[0] / y_res[1] if isinstance(y_res, tuple) else y_res
+                    )
                     xyz: dict = {
                         "x": x_res_value,
                         "y": y_res_value,
                         "z": z_physical_size,
-                        "unit": unit_name
+                        "unit": unit_name,
                     }
                     physical_sizes.update(xyz)
+            print(physical_sizes)
 
         elif file_extension in [".oir", ".nd2"]:
             image = BioImage(file_path)
@@ -61,7 +73,7 @@ def jwslab_load_bio_data(napari_viewer: napari.Viewer, file_path: Path,
             }
 
             # Assume the same unit is used on every axis
-            xyz["unit"] = getattr(metadata.pixels,"physical_size_x_unit")
+            xyz["unit"] = getattr(metadata.pixels, "physical_size_x_unit")
             physical_sizes.update(xyz)
 
         else:
@@ -78,7 +90,6 @@ def create_load_data_widget(viewer: napari.Viewer, physical_sizes: dict):
         file_path={"label": "Choose a file", "mode": "r"},
     )
     def select_bio_data_widget(file_path: Path = None):
-        jwslab_load_bio_data(viewer, file_path,physical_sizes)
-        
+        jwslab_load_bio_data(viewer, file_path, physical_sizes)
 
     return select_bio_data_widget